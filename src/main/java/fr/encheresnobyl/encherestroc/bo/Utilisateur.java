9901914f99cbd2765fcbf9deabb15faf6d61a992
rkerhir2021 <rkerhir2021@50SE11-2YTPT72.ad.campus-eni.fr>
2021-05-11 12:01:53 +0200

    utilisateur bo

--- src/main/java/fr/encheresnobyl/encherestroc/bo/Utilisateur.java
+++ src/main/java/fr/encheresnobyl/encherestroc/bo/Utilisateur.java
@@ -9,4 +9,203 @@ package fr.encheresnobyl.encherestroc.bo;
  */
 public class Utilisateur {
 
+	private boolean administrateur;
+	private int numeroUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private int codePostal;
+	private String ville;
+	private String MotDePasse;
+	
+	
+	/**
+	 * @param administrateur
+	 * @param numeroUtilisateur
+	 * @param pseudo
+	 * @param nom
+	 * @param prenom
+	 * @param email
+	 * @param telephone
+	 * @param rue
+	 * @param codePostal
+	 * @param ville
+	 * @param motDePasse
+	 */
+	public Utilisateur(boolean administrateur, int numeroUtilisateur, String pseudo, String nom, String prenom,
+			String email, String telephone, String rue, int codePostal, String ville, String motDePasse) {
+		this(pseudo,nom,prenom,email,telephone,rue,codePostal,ville,motDePasse);
+		setAdministrateur(administrateur);
+		setNumeroUtilisateur(numeroUtilisateur);		
+	}
+	
+	/**
+	 * @param pseudo
+	 * @param nom
+	 * @param prenom
+	 * @param email
+	 * @param telephone
+	 * @param rue
+	 * @param codePostal
+	 * @param ville
+	 * @param motDePasse
+	 */
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			int codePostal, String ville, String motDePasse) {
+		this();
+		setPseudo(pseudo);
+		setNom(nom);
+		setPrenom(prenom);
+		setEmail(email);
+		setTelephone(telephone);
+		setRue(rue);
+		setCodePostal(codePostal);
+		setVille(ville);
+		setMotDePasse(motDePasse);
+	}
+	/**
+	 * @return the administrateur
+	 */
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+
+	/**
+	 * @return the numeroUtilisateur
+	 */
+	public int getNumeroUtilisateur() {
+		return numeroUtilisateur;
+	}
+
+	/**
+	 * @param administrateur the administrateur to set
+	 */
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+
+	/**
+	 * @param numeroUtilisateur the numeroUtilisateur to set
+	 */
+	public void setNumeroUtilisateur(int numeroUtilisateur) {
+		this.numeroUtilisateur = numeroUtilisateur;
+	}
+
+	public Utilisateur() {
+		super();
+	}
+	/**
+	 * @return the pseudo
+	 */
+	public String getPseudo() {
+		return pseudo;
+	}
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+	/**
+	 * @return the rue
+	 */
+	public String getRue() {
+		return rue;
+	}
+	/**
+	 * @return the codePostal
+	 */
+	public int getCodePostal() {
+		return codePostal;
+	}
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+	/**
+	 * @return the motDePasse
+	 */
+	public String getMotDePasse() {
+		return MotDePasse;
+	}
+	/**
+	 * @param pseudo the pseudo to set
+	 */
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @param telephone the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	/**
+	 * @param rue the rue to set
+	 */
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	/**
+	 * @param codePostal the codePostal to set
+	 */
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	/**
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	/**
+	 * @param motDePasse the motDePasse to set
+	 */
+	public void setMotDePasse(String motDePasse) {
+		MotDePasse = motDePasse;
+	}
+	
+	
+	
 }
