52b09347cedd07b0defff48eb174f55e9b95940a
Mikael-LB <mikgitlab@gmail.com>
2021-05-11 16:20:05 +0200

    Minor change

---
+++ src/main/java/fr/encheresnobyl/encherestroc/bll/ArticleVenduManagerImpl.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package fr.encheresnobyl.encherestroc.bll;
+
+import java.util.List;
+
+import fr.encheresnobyl.encherestroc.bo.ArticleVendu;
+
+/**
+ * @author mlebris2021
+ * Class which implements the method of ArticleVenduManagerInt
+ */
+public class ArticleVenduManagerImpl implements ArticleVenduManagerInt {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<ArticleVendu> getByNom(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<ArticleVendu> getByNomAndCategorie(String nom, int noCategorie) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
