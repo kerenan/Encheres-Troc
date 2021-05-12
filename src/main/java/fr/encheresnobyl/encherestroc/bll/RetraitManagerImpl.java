aad3b80be66ffd70220da58144050972eb4107f9
Mikael-LB <mikgitlab@gmail.com>
2021-05-12 09:46:11 +0200

    Add Interface and create class for implementations of Enchere and Retrait Managers

---
+++ src/main/java/fr/encheresnobyl/encherestroc/bll/RetraitManagerImpl.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package fr.encheresnobyl.encherestroc.bll;
+
+/**
+ * @author mlebris2021
+ * Class which implements the method of RetraitManagerInt
+ */
+public class RetraitManagerImpl implements RetraitManagerInt {
+
+}
---
+++ src/main/java/fr/encheresnobyl/encherestroc/bll/RetraitManagerInt.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package fr.encheresnobyl.encherestroc.bll;
+
+/**
+ * @author mlebris2021
+ * Interface which define the method available for RetraitManagerImpl
+ */
+public interface RetraitManagerInt {
+
+}
